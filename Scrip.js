be1f832fbcfb99ac68a2e39bf26c598796a5b201
nabilasaf <nabilasafitri861@gmail.com>
2025-05-09 19:58:48 +0700

    revisi

--- Scrip.js
+++ Scrip.js
@@ -1,5 +1,9 @@
 window.onload = function() {
   setTimeout(() => {
     alert("🎉 Promo Spesial! Daftar sekarang & dapatkan diskon 30% untuk kelas pertama!");
+
+    // Setelah alert, kosongkan input form
+    const inputs = document.querySelectorAll("form input");
+    inputs.forEach(input => input.value = "");
   }, 1000);
 };
